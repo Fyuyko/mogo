397ae41e0943328e61742bdd7d0bdd610dd7f80f
Fyuyko <66683198+Fyuyko@users.noreply.github.com>
2020-06-09 23:58:45 +1000

    загрузили данные сайта

---
+++ js/app.js
@@ -0,0 +1,51 @@
+$(function() {
+
+    var header = $("#header"),
+        introH = $("#intro").innerHeight(),
+        scrollOffset = $(window).scrollTop ();
+    
+    /* Fixed header */ 
+    checkScroll(scrollOffset);
+
+    $(window).on("scroll", function (){
+
+        scrollOffset = $(this).scrollTop ();
+
+        checkScroll(scrollOffset)
+    });
+
+    function checkScroll(scrollOffset) {
+        
+        if( scrollOffset >= introH ) {
+            header.addClass("fixed");
+        } else {
+            header.removeClass("fixed")
+        }
+    }
+
+    /* Smooth scroll */ 
+    $("[data-scroll]").on("click", function(event) {
+        event.proventDefault();
+
+        var blockId = $(this).data('scroll'),
+            blockOffset = $(blockId).offset().top;
+
+        $("html, body").animate({
+            scrollTop: blockOffset
+        })
+    
+    });
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
